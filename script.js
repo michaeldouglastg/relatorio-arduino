d66caf5e218079dbe35f3410a70691b710655e6e
Michael Douglas <michaeldouglas835@gmail.com>
2024-09-10 01:43:55 -0300

    update layout

--- script.js
+++ script.js
@@ -13,9 +13,9 @@ function toggleMode() {
   //substituir a imagem
   if(html.classList.contains('light')) {
     //se tiver light mode, adiciona a imgem light
-    img.setAttribute("src", "./assets/avatar-light.png")
+    img.setAttribute("src", "./assets/logo-light.png")
   } else {
     //se tiver sem light mode, manter a imagem normal
-    img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute("src", "./assets/logo-dark.png")
   }
 }
