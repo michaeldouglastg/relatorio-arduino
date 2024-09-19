6cbb1f2056c01954974d97fa73791c4cbbef4744
Michael Douglas <michaeldouglas835@gmail.com>
2024-09-19 00:58:53 -0300

    update layout

--- pages/teste.js
+++ pages/teste.js
@@ -1,3 +1,37 @@
+//Botão Slides
+let next = document.querySelector('.next');
+let prev = document.querySelector('.prev');
+let slider = document.querySelector('.slider');
+
+next.addEventListener('click', function(){
+    let slides = document.querySelectorAll('.slides');
+    slider.appendChild(slides[0]);
+})
+
+prev.addEventListener('click', function(){
+    let slides = document.querySelectorAll('.slides');
+    slider.prepend(slides[slides.length - 1]);
+})
+
+//Alternar Modo Escuro
+function toggleMode() {
+    const html = document.documentElement
+    html.classList.toggle("light")
+    
+    //pegar a tag img
+    const img=document.querySelector("#profile img")
+    
+    //substituir a imagem
+    if(html.classList.contains('light')) {
+      //se tiver light mode, adiciona a imgem light
+      img.setAttribute("src", "./assets/avatar-light.png")
+    } else {
+      //se tiver sem light mode, manter a imagem normal
+      img.setAttribute("src", "./assets/avatar.png")
+    }
+  }
+
+
 //Função do Botões de Navegação
 function initializeTabContainer(tabContainer) {
   const tabButtons = tabContainer.querySelectorAll('.tab-button');
