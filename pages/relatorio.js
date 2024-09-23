dccd84b7e0f16b608f7ecbff7a145f95a3374231
Michael Douglas <michaeldouglas835@gmail.com>
2024-09-23 17:05:03 -0300

    restructured styles

--- pages/relatorio.js
+++ pages/relatorio.js
@@ -97,3 +97,83 @@ function initializeTabContainer(tabContainer) {
 
 // Initialize all tab containers
 document.querySelectorAll('.tab-container').forEach(initializeTabContainer);
+
+
+
+//Horizontal Slider Nav
+const tabs = document.querySelectorAll(".tab-container li");
+const rightArrow = document.querySelector(
+  ".tab-container .right-arrow svg"
+);
+const leftArrow = document.querySelector(
+  ".tab-container .left-arrow svg"
+);
+const tabsList = document.querySelector(".tab-container ul");
+const leftArrowContainer = document.querySelector(
+  ".tab-container .left-arrow"
+);
+const rightArrowContainer = document.querySelector(
+  ".tab-container .right-arrow"
+);
+
+const removeAllActiveClasses = () => {
+  tabs.forEach((tab) => {
+    tab.classList.remove("active");
+  });
+};
+
+tabs.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    removeAllActiveClasses();
+    tab.classList.add("active");
+  });
+});
+
+const manageIcons = () => {
+  if (tabsList.scrollLeft >= 20) {
+    leftArrowContainer.classList.add("active");
+  } else {
+    leftArrowContainer.classList.remove("active");
+  }
+
+  let maxScrollValue = tabsList.scrollWidth - tabsList.clientWidth - 20;
+  console.log("scroll width: ", tabsList.scrollWidth);
+  console.log("client width: ", tabsList.clientWidth);
+
+  if (tabsList.scrollLeft >= maxScrollValue) {
+    rightArrowContainer.classList.remove("active");
+  } else {
+    rightArrowContainer.classList.add("active");
+  }
+};
+
+rightArrow.addEventListener("click", () => {
+  tabsList.scrollLeft += 200;
+  manageIcons();
+});
+
+leftArrow.addEventListener("click", () => {
+  tabsList.scrollLeft -= 200;
+  manageIcons();
+});
+
+tabsList.addEventListener("scroll", manageIcons);
+
+let dragging = false;
+
+const drag = (e) => {
+  if (!dragging) return;
+  tabsList.classList.add("dragging");
+  tabsList.scrollLeft -= e.movementX;
+};
+
+tabsList.addEventListener("mousedown", () => {
+  dragging = true;
+});
+
+tabsList.addEventListener("mousemove", drag);
+
+document.addEventListener("mouseup", () => {
+  dragging = false;
+  tabsList.classList.remove("dragging");
+});
